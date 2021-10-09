5c22d0042446ac16eb250a2fda2c43d2137a37c0
Eric <menaericdaniel@gmail.com>
2021-10-09 17:28:56 -0300

    apply map

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ import { Fragment } from 'react';
 import Curso from './components/Curso';
 import './styles/styles.scss';
 
+const courses = [
+  {
+    title : "React desde cero",
+    image : "https://ed.team/_next/image?url=https%3A%2F%2Fedteam-media.s3.amazonaws.com%2Fcourses%2Fbig%2F7204fcde-37aa-49a2-a619-63459f834ada.png&w=1920&q=75",
+    price : "50"
+  },
+  {
+    title : "Formularios con React",
+    image : "https://ed.team/_next/image?url=https%3A%2F%2Fedteam-media.s3.amazonaws.com%2Fcourses%2Fbig%2Fbea68093-bc25-4492-8244-b9c2a1b31e26.png&w=1920&q=75",
+    price : "20"
+  },
+  {
+    title : "React native",
+    image : "https://ed.team/_next/image?url=https%3A%2F%2Fedteam-media.s3.amazonaws.com%2Fcourses%2Fbig%2F9cfa2a82-e1d9-4903-849f-b3d09804a703.png&w=1920&q=75",
+    price : "60"
+  }
+]
+
 const App = () => (
   <Fragment>
   <div className="main-banner img-container l-section" id="main-banner">
@@ -19,21 +37,17 @@ const App = () => (
     </div>
   </div>
   <div className="ed-grid m-grid-3">
-    <Curso 
-      title="React desde cero" 
-      image="https://ed.team/_next/image?url=https%3A%2F%2Fedteam-media.s3.amazonaws.com%2Fcourses%2Fbig%2F7204fcde-37aa-49a2-a619-63459f834ada.png&w=1920&q=75" 
-      price="50"
+
+    {courses.map(course => (
+
+      <Curso 
+        title = {course.title}
+        image = {course.image}
+        price = {course.price}
       />
-    <Curso 
-    title="Formularios con React" 
-    image="https://ed.team/_next/image?url=https%3A%2F%2Fedteam-media.s3.amazonaws.com%2Fcourses%2Fbig%2Fbea68093-bc25-4492-8244-b9c2a1b31e26.png&w=1920&q=75" 
-    price="20"
-    />
-    <Curso 
-    title="React native" 
-    image="https://ed.team/_next/image?url=https%3A%2F%2Fedteam-media.s3.amazonaws.com%2Fcourses%2Fbig%2F9cfa2a82-e1d9-4903-849f-b3d09804a703.png&w=1920&q=75" 
-    price="60"
-    />
+
+    ))}
+  
 
 </div>
 </Fragment>

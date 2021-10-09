04263fe2fc438ce3633b90d211232eb4b362ab1e
Eric <menaericdaniel@gmail.com>
2021-10-09 16:31:14 -0300

    add props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 //import logo from './logo.svg';
 //import './App.css';
 import { Fragment } from 'react';
-import Curso from './components/Curso'
-import './styles/styles.scss'
+import Curso from './components/Curso';
+import './styles/styles.scss';
 
 const App = () => (
   <Fragment>
@@ -19,9 +19,21 @@ const App = () => (
     </div>
   </div>
   <div className="ed-grid m-grid-3">
-    <Curso />
-    <Curso />
-    <Curso />
+    <Curso 
+      title="React desde cero" 
+      image="https://ed.team/_next/image?url=https%3A%2F%2Fedteam-media.s3.amazonaws.com%2Fcourses%2Fbig%2F7204fcde-37aa-49a2-a619-63459f834ada.png&w=1920&q=75" 
+      price="50"
+      />
+    <Curso 
+    title="Formularios con React" 
+    image="https://ed.team/_next/image?url=https%3A%2F%2Fedteam-media.s3.amazonaws.com%2Fcourses%2Fbig%2Fbea68093-bc25-4492-8244-b9c2a1b31e26.png&w=1920&q=75" 
+    price="20"
+    />
+    <Curso 
+    title="React native" 
+    image="https://ed.team/_next/image?url=https%3A%2F%2Fedteam-media.s3.amazonaws.com%2Fcourses%2Fbig%2F9cfa2a82-e1d9-4903-849f-b3d09804a703.png&w=1920&q=75" 
+    price="60"
+    />
 
 </div>
 </Fragment>
